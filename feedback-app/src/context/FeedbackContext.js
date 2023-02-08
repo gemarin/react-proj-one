b839db06d0c9624d783098a579fb8de7fb27a409
Gaby M <gmarin1997@gmail.com>
2023-02-07 19:02:07 -0600

    [patch]: these changes too

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -14,12 +14,27 @@ export const FeedbackProvider = ({
   const [feedback, setFeedback] =
     useState(FeedbackData);
 
+  const [
+    editedFeedback,
+    setEditedFeedback,
+  ] = useState({
+    item: {},
+    edit: false,
+  });
+
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([
-      newFeedback,
       ...feedback,
+      newFeedback,
     ]);
+  };
+
+  const editFeedback = (item) => {
+    setEditedFeedback({
+      item,
+      edit: true,
+    });
   };
 
   const deleteFeedback = (id) => {
@@ -35,12 +50,29 @@ export const FeedbackProvider = ({
       );
     }
   };
+
+  const updateFeedback = (
+    id,
+    newItem
+  ) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id
+          ? { ...item, ...newItem }
+          : item
+      )
+    );
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        editedFeedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
